44d8ba130f0e6c09236289a86f3f398358af534a
e5dran <esdran.bc@gmail.com>
2022-12-08 12:52:14 +0100

    creacion de ruta api y ruta user, con su configuracion correspondiente en app.js

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 app.use(logger('dev'));
@@ -14,7 +11,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', require('./routes/api'));
 
 module.exports = app;
