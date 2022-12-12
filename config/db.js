fa88636fc55f82616536ab25df5ffbeb51692c9b
piki1489 <piki1489@hotmail.com>
2022-12-12 09:25:06 +0100

    Cambio de contraseña bd

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'MySQLchandrian7',
+    password: 'Lizarra1489@',
     port: '3306',
     database: 'proyecto_almacen'
 });
