5161e530cdaf8e22972f81ace3f65d6ce9137246
e5dran <esdran.bc@gmail.com>
2022-12-12 10:59:49 +0100

    Creadas las Querys de user en users.model (getById, create, update, deleteById)

--- models/user.model.js
+++ models/user.model.js
@@ -2,6 +2,27 @@ const getAll = () => {
     return db.query('select * from users');
 }
 
+const getById = (userId) => {
+    return db.query('select * from users where id = ?', [userId]);
+}
+
+const create = ({ name, surname, email, password, phone, adress, dni, gender, category }) => {
+    return db.query('insert into users (name, surname, email, password, phone, adress, dni, gender, category) values (  ?, ?, ?, ?, ?, ?, ?, ?, ?)', [name, surname, email, password, phone, adress, dni, gender, category]);
+}
+
+const update = (userId, { name, surname, email, password, phone, adress, dni, gender, category }) => {
+    return db.query('update users set name= ?, surname= ?, email= ?, password= ?, phone= ?, adress= ?, dni= ?, gender= ?, category= ? where id = ?', [name, surname, email, password, phone, adress, dni, gender, category, userId]);
+}
+
+const deleteById = (userId) => {
+    return db.query('delete from users where id = ?', [userId]);
+}
+
+
 module.exports = {
-    getAll
+    getAll,
+    getById,
+    create,
+    update,
+    deleteById
 }
