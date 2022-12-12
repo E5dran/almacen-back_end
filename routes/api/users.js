b3e916af48edf5a5342288a51a9ea5ac6fd57a33
e5dran <esdran.bc@gmail.com>
2022-12-12 11:16:50 +0100

    Creadas peticiones post, put y delete en users y sus comprobaciones en peticiones.rest

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,41 @@
-const { getAll } = require('../../models/user.model');
+const { getAll, create, update, deleteById } = require('../../models/user.model');
 
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
     try {
         const [user] = await getAll();
+        res.json(user);
+    } catch (error) {
+        res.json({ fatal: error.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const [user] = await create(req.body);
+        res.json(user)
+    } catch (error) {
+        res.json({ fatal: error.message });
+    }
+});
+
+router.put('/:userId', async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const [user] = await update(userId, req.body);
+        res.json(user);
+    } catch (error) {
+        res.json({ fatal: error.message });
+    }
+});
+
+router.delete('/:userId', async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const [user] = await deleteById(userId);
         res.json(user);
     } catch (error) {
         res.json({ fatal: error.message });
