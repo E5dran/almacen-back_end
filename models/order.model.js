819dbc1089b7d470f3ac01617c1c3eb1525ee141
e5dran <esdran.bc@gmail.com>
2022-12-14 16:31:54 +0100

    Creadas Querys de busqueda en order.model mas especificas (getByWarehouseId, getByWarehouseIdStatus, getByWarehouseIdStatusCat)

--- models/order.model.js
+++ models/order.model.js
@@ -14,12 +14,23 @@ const update = (ordersId, { addressee, description, n_items, destination_address
     return db.query('update orders set addressee= ?, description= ?, n_items= ?, destination_address= ?,  departure_date= ?, arrival_date= ?, truck_plate= ?, category= ? where id = ?', [addressee, description, n_items, destination_address, departure_date, arrival_date, truck_plate, category, ordersId])
 
 }
-//Este borra por ID ver si está bien
+
 const deleteById = (ordersId) => {
     return db.query('delete from orders where id = ?', [ordersId]);
 }
 
+const getByWarehouseId = (warehouseId) => {
+    return db.query('select * from orders where warehouse_id = ?', [warehouseId]);
+}
+
+const getByWarehouseIdStatus = (warehouseId, status) => {
+    return db.query('select * from orders where warehouse_id = ? && status = ?', [warehouseId, status]);
+}
+
+const getByWarehouseIdStatusCat = (warehouseId, status, cat) => {
+    return db.query('select * from orders where warehouse_id = ? && status = ? && category = ?', [warehouseId, status, cat]);
+}
 
 module.exports = {
-    getAll, create, update, deleteById, getById
+    getAll, create, update, deleteById, getById, getByWarehouseId, getByWarehouseIdStatus, getByWarehouseIdStatusCat
 }
